f59425794192ece397c0303854fcdf0b37aa7b32
marechal <152326728+Dam-s@users.noreply.github.com>
2026-06-21 13:05:37 -0400

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def threeSum(self, liste: List[int]) -> List[List[int]]:
+        resultats = []
+        deja_vus = set()
+
+        # Créer un dictionnaire qui mappe chaque valeur à ses indices
+        valeur_indices = {}
+        for idx, val in enumerate(liste):
+            if val not in valeur_indices:
+                valeur_indices[val] = []
+            valeur_indices[val].append(idx)
+
+
+        def sont_opposes(a, b):
+            return a + b == 0
+
+
+        for i in range(len(liste)):
+            for j in range(i + 1, len(liste)):
+                cible = -(liste[i] + liste[j])
+                
+                # Vérifier si la cible existe dans la liste avec un indice différent de i et j
+                if cible in valeur_indices:
+                    for k in valeur_indices[cible]:
+                        if k != i and k != j:
+                            sous_liste = sorted([liste[i], liste[j], cible])
+                            cle = tuple(sous_liste)
+                            
+                            if cle not in deja_vus:
+                                deja_vus.add(cle)
+                                resultats.append(sous_liste)
+                            break
+        return resultats
